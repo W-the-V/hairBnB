85e87c6b537d57e17ec30861c5e09f326e02bc50
walt <walter.hills5@gmail.com>
2021-02-20 10:29:00 -0600

    Styling

---
+++ frontend/src/components/LandingShell/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./LandingShell.css";
+
+function LandingShell() {
+  return (
+    <div className="LandingShell">
+      <div className="localBox">
+        <p className="largeText">Shop Local</p>
+        <button className="menuButton">Explore local options</button>
+      </div>
+    </div>
+  );
+}
+
+export default LandingShell;
