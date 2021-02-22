e3bbf8e900514b6052a97c81901bbf93da9b3bd9
walt <walter.hills5@gmail.com>
2021-02-22 14:14:46 -0600

    modal Styling

--- frontend/src/components/TravelSection/index.js
+++ frontend/src/components/TravelSection/index.js
@@ -5,10 +5,12 @@ function TravelSection() {
       <h2 className="travelTitle">Travel</h2>
       <div className="travelShell">
         <div className="travelBlock">
-          <div className="travelImage">IMAGE HERE</div>
-          <div className="travelImage">IMAGE HERE</div>
-          <div className="travelImage">IMAGE HERE</div>
-          <div className="travelImage">IMAGE HERE</div>
+          <div className="travelImageShell">
+            <div className="travelImage">IMAGE HERE</div>
+            <div className="travelImage">IMAGE HERE</div>
+            <div className="travelImage">IMAGE HERE</div>
+            <div className="travelImage">IMAGE HERE</div>
+          </div>
         </div>
       </div>
     </>
