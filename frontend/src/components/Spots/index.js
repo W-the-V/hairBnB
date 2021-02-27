d6e5cc7e42112f1f040eff73dfcba05fedcad50e
walt <walter.hills5@gmail.com>
2021-02-27 14:40:28 -0600

    styling

--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -8,13 +8,28 @@ function Spots({ spot }) {
     <button className="spotBox" onClick={(e) => dispatch(showSpot(id))}>
       <div className="spotImage">IMAGE HERE</div>
       <div className="spotInner">
-        <div className="spotType">
-          {spot.type} at {spot.address}
+        <div className="partialLineBox">
+          <div className="spotType">
+            {spot.type} at {spot.address}
+          </div>
         </div>
-        <div className="spotTitle">{spot.name}</div>
-        <div className="amenitiesList"> List of amenities</div>
-        <div className="spotRating"> Star, Rating, review number</div>
-        <div className="spotPrice">${spot.price}</div>
+        <div className="partialLineBox">
+          <div className="spotTitle">{spot.name}</div>
+        </div>
+        <div className="partialLineBox">
+          <div className="partialLine"></div>
+        </div>
+        <div className="partialLineBox">
+          <div className="amenitiesList"> List of amenities</div>
+        </div>
+        <div className="ratingPriceBox">
+          <div className="spotRating">
+            {" "}
+            <i className="fas fa-star"></i>
+            {"5   (100)"}
+          </div>
+          <div className="spotPrice">${spot.price}</div>
+        </div>
       </div>
     </button>
   );
